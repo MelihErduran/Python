3804809be80073199c80b14faeec3420061cd50c
Melih Erduran <merduran.ed@gmail.com>
2022-10-31 13:00:39 -0700

    Nice:

---
+++ new.py
@@ -0,0 +1,7 @@
+print('Hello Welcome')
+answer = input('What Would You Like to Drink?')
+
+if answer == 'Coffee' or 'coffee':
+    print('Okay, We Will Bring That Out Shortly')
+else:
+    print('We Don\'t Have that')
